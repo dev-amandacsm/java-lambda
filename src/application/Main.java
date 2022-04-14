7caece3cbf9c66598fb5435b80bf556729df128b
Amanda Muniz <dev.amandacsm@gmail.com>
2022-04-14 13:49:20 -0300

    V5 - Expressão lambda inline

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,9 +20,7 @@ public class Main {
 
         double min = 100.0;
 
-        Predicate<Product> predicate = p -> p.getPrice() >= min;
-
-        list.removeIf(predicate);
+        list.removeIf(p -> p.getPrice() >= min);
 
         for(Product p : list) System.out.println(p);
     }
