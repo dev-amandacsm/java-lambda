a83d624b3d59dc119e29fe7e71bb167ee43b9b61
Amanda Muniz <dev.amandacsm@gmail.com>
2022-04-14 13:48:11 -0300

    V4 - Expressão lambda declarada

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import util.ProductPredicate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,7 +18,11 @@ public class Main {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product :: nonStaticProductPredicate);
+        double min = 100.0;
+
+        Predicate<Product> predicate = p -> p.getPrice() >= min;
+
+        list.removeIf(predicate);
 
         for(Product p : list) System.out.println(p);
     }
